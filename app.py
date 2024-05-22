2e23936131f1d52eaf3e8bde025be219215f0e83
Daniel <danielcd3099@gmail.com>
2024-05-22 20:52:29 -0300

    Solved interface button issue

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ DESCRIPTION_TEXT = f"Esta é uma aplicação para o trabalho de NLP. Utilizamos 
 st.title(TITLE_TEXT)
 st.write(DESCRIPTION_TEXT)
 
-df = pd.read_csv('data/imdb_reviews.csv')
-df = df.head()
-df['preprocessed_review'] = df['review'].copy()
-df['preprocessed_review'] = df['preprocessed_review'].apply(preprocess_text)
-cols = ['review','preprocessed_review','sentiment']
-st.table(df[cols])
+if st.button('Encontre exemplos!'):
+    df = pd.read_csv('data/imdb_reviews.csv')
+    df = df.sample(5)
+    df['preprocessed_review'] = df['review'].copy()
+    df['preprocessed_review'] = df['preprocessed_review'].apply(preprocess_text)
+    cols = ['review','preprocessed_review','sentiment']    
+
+    st.table(df[cols])
